e26eb521f4c6c9e1b0abea71f358d6cff11cd063
Darek Radke <dradmclad@gmail.com>
2022-07-13 00:40:02 -0500

    burger btn

---
+++ src/components/HamburgerMenuBtn/HamburgerMenuBtn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HamburgerMenuBtn.scss";
+
+const HamburgerMenuBtn = props => {
+    return (
+      <div className="burger-wrapper">
+        <div className="burger-btn" />
+      </div>
+    );
+  }
+
+export default HamburgerMenuBtn;
