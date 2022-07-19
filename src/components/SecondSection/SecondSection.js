c1e02b8d7ba55cbdfda41d5105e383de5aecf81d
Darek Radke <dradmclad@gmail.com>
2022-07-19 18:30:28 -0500

    font sizes and carousel css

--- src/components/SecondSection/SecondSection.js
+++ src/components/SecondSection/SecondSection.js
@@ -6,6 +6,24 @@ const SecondSection = (props) => {
   return (
     <div className="second-section" ref={props.secondRef}>
       <Carousel />
+      <div className="carousel-blurbs">
+        <p>
+          In a world full of varying devices responsive web design is a very
+          important aspect of my development approach. Dynamic elements and CSS
+          queries ensure that users will have a high quality experience no
+          matter the screen size.
+        </p>
+        <p>
+          The utilization of frameworks, libraries, and APIs aid in the creation
+          of an optomized web development project.
+        </p>
+        <p>
+          The world of website design and development is fast paced. Being able
+          to learn quickly and staying up to date with the best possible
+          solutions ensures that anything I create adheres to the highest
+          standards.
+        </p>
+      </div>
     </div>
   );
 };
