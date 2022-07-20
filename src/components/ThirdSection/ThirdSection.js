c213d6cc6c5b4b3c00440a4c67cbeb4da52f4f29
Darek Radke <dradmclad@gmail.com>
2022-07-20 16:26:43 -0500

    projects section updated

--- src/components/ThirdSection/ThirdSection.js
+++ src/components/ThirdSection/ThirdSection.js
@@ -4,7 +4,21 @@ import "./ThirdSection.scss";
 const ThirdSection = (props) => {
   return (
     <div className="third-section" ref={props.thirdRef}>
-
+      <div className="project-card">
+        <p className="card-title">Custom CMS</p>
+        <p className="card-blurb">Application that allows a user to update front end styling.</p>
+        <a href="https://github.com/DarekR88/cafe-del-sol" className="card-link">repository</a>
+      </div>
+      <div className="project-card">
+        <p className="card-title">Data Graphing Application</p>
+        <p className="card-blurb">Application that pulls multiple sets of data from an api and graphs them together using Recharts</p>
+        <a href="https://github.com/DarekR88/eog-react-visualization" className="card-link">repository</a>
+      </div>
+      <div className="project-card">
+        <p className="card-title">React Portfolio</p>
+        <p className="card-blurb">Portfolio created with React and react-image-gallery.</p>
+        <a href="https://github.com/DarekR88/portfolio" className="card-link">repository</a>
+      </div>
     </div>
   );
 };
