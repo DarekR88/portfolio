422ce3757b60ac216ac85b69a21604f8b00df735
Darek Radke <dradmclad@gmail.com>
2022-07-20 20:52:57 -0500

    third section css

--- src/components/ThirdSection/ThirdSection.js
+++ src/components/ThirdSection/ThirdSection.js
@@ -4,20 +4,42 @@ import "./ThirdSection.scss";
 const ThirdSection = (props) => {
   return (
     <div className="third-section" ref={props.thirdRef}>
-      <div className="project-card">
-        <p className="card-title">Custom CMS</p>
-        <p className="card-blurb">Application that allows a user to update front end styling.</p>
-        <a href="https://github.com/DarekR88/cafe-del-sol" className="card-link">repository</a>
-      </div>
-      <div className="project-card">
-        <p className="card-title">Data Graphing Application</p>
-        <p className="card-blurb">Application that pulls multiple sets of data from an api and graphs them together using Recharts</p>
-        <a href="https://github.com/DarekR88/eog-react-visualization" className="card-link">repository</a>
-      </div>
-      <div className="project-card">
-        <p className="card-title">React Portfolio</p>
-        <p className="card-blurb">Portfolio created with React and react-image-gallery.</p>
-        <a href="https://github.com/DarekR88/portfolio" className="card-link">repository</a>
+      <p className="section-title">Projects</p>
+      <div className="card-wrapper">
+        <div className="project-card">
+          <p className="card-title">Custom CMS</p>
+          <p className="card-blurb">
+            Application that allows a user to update front end styling.
+          </p>
+          <a
+            href="https://github.com/DarekR88/cafe-del-sol"
+            className="card-link"
+          >
+            repository
+          </a>
+        </div>
+        <div className="project-card">
+          <p className="card-title">Data Graphing Application</p>
+          <p className="card-blurb">
+            Application that pulls multiple sets of data from an api and graphs
+            them together using Recharts
+          </p>
+          <a
+            href="https://github.com/DarekR88/eog-react-visualization"
+            className="card-link"
+          >
+            repository
+          </a>
+        </div>
+        <div className="project-card">
+          <p className="card-title">React Portfolio</p>
+          <p className="card-blurb">
+            Portfolio created with React and react-image-gallery.
+          </p>
+          <a href="https://github.com/DarekR88/portfolio" className="card-link">
+            repository
+          </a>
+        </div>
       </div>
     </div>
   );
