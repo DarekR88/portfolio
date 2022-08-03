b2d9739c91d4ebd5966877257f3fd9e182bc15c2
Darek Radke <dradmclad@gmail.com>
2022-08-03 14:12:00 -0500

    project section css

--- src/components/ThirdSection/ThirdSection.js
+++ src/components/ThirdSection/ThirdSection.js
@@ -9,59 +9,79 @@ const ThirdSection = (props) => {
       <p className="section-title">Projects</p>
       <div className="card-wrapper">
         <div className="project-card">
-          <img className="folder-icon" src={folderIcon} alt="folder icon" />
+          <header className="project-card-header">
+            <a
+              href="https://github.com/DarekR88/cafe-del-sol"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img className="folder-icon" src={folderIcon} alt="folder icon" />
+            </a>
+            <a
+              href="https://github.com/DarekR88/cafe-del-sol"
+              className="card-link"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img className="repo-icon" src={githubIcon} alt="github icon" />
+            </a>
+          </header>
           <p className="card-title">Custom CMS</p>
           <p className="card-blurb">
             Application that allows a user to update front end styling.
           </p>
-          <p className="technologies">
-            JavaScript, React 
-          </p>
-          <a
-            href="https://github.com/DarekR88/cafe-del-sol"
-            className="card-link"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <img className="repo-icon" src={githubIcon} alt="github icon" />
-          </a>
+          <p className="technologies">JavaScript, React</p>
         </div>
         <div className="project-card">
-        <img className="folder-icon" src={folderIcon} alt="folder icon" />
+          <header className="project-card-header">
+            <a
+              href="https://github.com/DarekR88/eog-react-visualization"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img className="folder-icon" src={folderIcon} alt="folder icon" />
+            </a>{" "}
+            <a
+              href="https://github.com/DarekR88/eog-react-visualization"
+              className="card-link"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img className="repo-icon" src={githubIcon} alt="github icon" />
+            </a>
+          </header>
+
           <p className="card-title">Data Graphing Application</p>
           <p className="card-blurb">
             Application that pulls multiple sets of data from an api and graphs
             them together using Recharts
           </p>
-          <p className="technologies">
-            JavaScript, React, Redux, Recharts
-          </p>
-          <a
-            href="https://github.com/DarekR88/eog-react-visualization"
-            className="card-link"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <img className="repo-icon" src={githubIcon} alt="github icon" />
-          </a>
+          <p className="technologies">JavaScript, React, Redux, Recharts</p>
         </div>
         <div className="project-card">
-        <img className="folder-icon" src={folderIcon} alt="folder icon" />
+          <header className="project-card-header">
+            <a
+              href="https://github.com/DarekR88/portfolio"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img className="folder-icon" src={folderIcon} alt="folder icon" />
+            </a>{" "}
+            <a
+              href="https://github.com/DarekR88/portfolio"
+              className="card-link"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img className="repo-icon" src={githubIcon} alt="github icon" />
+            </a>
+          </header>
+
           <p className="card-title">React Portfolio</p>
           <p className="card-blurb">
             Portfolio created with React and react-image-gallery.
           </p>
-          <p className="technologies">
-            JavaScript, React, react-image-gallery
-          </p>
-          <a
-            href="https://github.com/DarekR88/portfolio"
-            className="card-link"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <img className="repo-icon" src={githubIcon} alt="github icon" />
-          </a>
+          <p className="technologies">JavaScript, React, react-image-gallery</p>
         </div>
       </div>
     </div>
