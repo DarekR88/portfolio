cacf77efd3ab5c22f4479eda177226806c71b04d
Darek Radke <dradmclad@gmail.com>
2022-08-04 11:41:02 -0500

    footer icons removed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,10 @@
 import React from "react";
 import "./Footer.scss";
-import githubIcon from "../../static/icons/github_icon.svg";
-import linkedinIcon from "../../static/icons/linkedin_icon.svg";
 
 const Footer = (props) => {
   return (
     <div className="footer">
       <p className="footer-blurb">Created by Darek Radke</p>
-      <a
-        href="https://github.com/DarekR88"
-        target="_blank"
-        rel="noreferrer noopener"
-        className="side-a"
-      >
-        <img className="svg-icon" src={githubIcon} alt="github icon" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/darek-radke-aa26b7173/"
-        target="_blank"
-        rel="noreferrer noopener"
-        className="side-a"
-      >
-        <img className="svg-icon" src={linkedinIcon} alt="linkedin icon" />
-      </a>
     </div>
   );
 };
