1b139d163ee864dcfa4027a59612489e4a7f681d
Darek Radke <dradmclad@gmail.com>
2023-10-26 17:41:03 -0500

    dark/light mode color change for first section

--- src/components/FirstSection/FirstSection.js
+++ src/components/FirstSection/FirstSection.js
@@ -2,8 +2,17 @@ import React from "react";
 import "./FirstSection.scss";
 
 const FirstSection = (props) => {
+
+  let sectionColor = "dark"
+  
+  if (props.darkLight) {
+    sectionColor = "dark"
+  } else {
+    sectionColor = "light"
+  }
+
   return (
-    <div className="first-section" ref={props.landingRef}>  
+    <div className={ `first-section ${ sectionColor }` } ref={props.landingRef}>  
         <p className="text">Hi, I'm</p>
         <p className="name">Darek Radke</p>
         <p className="blurb">Independent website developer.</p> 
