d5f2cb2796da5bb0a3f66dfe58c6c2383ba451f7
Darek Radke <dradmclad@gmail.com>
2024-01-25 12:08:53 -0600

    profile updated

--- src/components/FourthSection/FourthSection.js
+++ src/components/FourthSection/FourthSection.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./FourthSection.scss";
-import Modal from "../Modal/Modal";
+// import Modal from "../Modal/Modal";
 
 const FourthSection = (props) => {
-  let modal;
-  let darkLight = "dark"
+  // let modal;
+  let darkLight = "dark";
 
   if (props.darkLight) {
     darkLight = "dark";
@@ -12,25 +12,33 @@ const FourthSection = (props) => {
     darkLight = "light";
   }
 
-  if (props.modalOpen) {
-    modal = (
-      <Modal modalOpen={props.modalOpen} toggleModal={props.toggleModal} />
-    );
-  }
+  // if (props.modalOpen) {
+  //   modal = (
+  //     <Modal modalOpen={props.modalOpen} toggleModal={props.toggleModal} />
+  //   );
+  // }
 
   return (
-    <div className={ `fourth-section ${ darkLight }` } ref={props.fourthRef}>
+    <div className={`fourth-section ${darkLight}`} ref={props.fourthRef}>
       <div className="section-container">
-        {modal}
+        {/* {modal} */}
         <p className="section-title left">Contact</p>
         <p className="contact-me text">
-          I am always looking for the best fit for my skill set and to
-          collaborate on interesting projects. Send me a message and I'll get
-          back to you as soon as possible.
+          I&#39;m always looking for new opportunities to level-up my skill sets
+          and collaborate with others on exciting projects. Send me a message,
+          and I&#39;ll get back to you as soon as possible.
         </p>
-        <button onClick={props.toggleModal} className="modal-button">
+        {/* <button onClick={props.toggleModal} className="modal-button">
           Contact Me
-        </button>
+        </button> */}
+        <a
+          href="mailto:darekradke2@gmail.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="modal-button"
+        >
+          Contact Me
+        </a>
       </div>
     </div>
   );
